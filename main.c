257cb5c1811cae83c914b4d9e89e49b4dd6121ad
Meghna <105413481+MeghnaAG@users.noreply.github.com>
2023-03-30 09:30:20 +0530

    Array sum

--- main.c
+++ main.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int isprime(int n)
+{
+    int i,flag=1;
+    for(i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            flag=0;
+            break;
+        }
+    }
+    return flag;
+}
 int main()
 {
-    int a[10],i,j,temp;
+    int a[10],i,sum=0,r;
     printf("Enter a 10 digit array:");
     for(i=0;i<10;i++)
     {
         scanf("%d",&a[i]);
+        sum+=a[i];
     }
-    //insertion sort
-    for(i=1;i<10;i++)
-    {
-        j=i-1;
-        temp=a[i];
-        if(a[j]>a[j+1])
-        {
-            while(j>=0)
-            {
-                a[j+1]=a[j];
-                j--;
-            }
-            a[j+1]=temp;
-        }
-    }
-    printf("Sorted array:");
-    for(i=0;i<10;i++)
-        printf("%d ",a[i]);
+    r=isprime(sum);
+    if(r==1)
+        printf("Sum is prime");
+    else
+        printf("Sum is not prime");
     return 0;
 }
